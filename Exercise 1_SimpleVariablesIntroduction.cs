26cd4463ea4ed8d3b61f92d08a0b5ca3fdf26f2a
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2024-06-03 00:35:46 +0800

    Add files via upload
    Udemy - Complete C# Masterclass

---
+++ Exercise 1_SimpleVariablesIntroduction.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public void PrintNumber()
+        {
+            // TODO
+            int number = 10;
+            Console.WriteLine(number);
+        }
+    }
+}
