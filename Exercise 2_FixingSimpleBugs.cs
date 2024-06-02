6f8edcedd7a2194b70a6cf126d55ee684b9fa262
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2024-06-03 00:38:05 +0800

    Add files via upload
    Udemy - Complete C# Masterclass

---
+++ Exercise 2_FixingSimpleBugs.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public void CalculateAverage()
+        {
+            int num1 = 10;
+            int num2 = 20;
+            int num3 = 30;
+            int average = (num1 + num2 + num3) / 3;
+            Console.WriteLine("The average is: " + average);
+        }
+    }
+}
