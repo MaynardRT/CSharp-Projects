ba51d3c8fc57eee85851d393c72960be837439c0
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2024-06-03 00:41:29 +0800

    Add files via upload
    Udemy - Complete C# Masterclass

---
+++ Exercise 3_SpecialChractersandEscapeSequences.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public void PrintSpecialCharacters()
+        {
+            // TODO
+            Console.WriteLine("Hello\n\tWorld!");
+        }
+    }
+}
