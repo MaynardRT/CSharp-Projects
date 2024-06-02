90f2ee6466bc0cad2e73760aff06841e63bc216d
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2024-06-03 01:32:15 +0800

    Add files via upload
    Udemy - Complete C# Masterclass

---
+++ Exercise 5_UsingSwitchStatements.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public void PrintDay()
+        {
+            int day = 3;
+
+            // TODO: Rewrite this if-else structure using a switch statement.
+            switch (day) {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+        }
+    }
+}
