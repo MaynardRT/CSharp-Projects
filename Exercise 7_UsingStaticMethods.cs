34ac7cc77217efcbe76e101f38672ac3c1ce2da8
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2024-06-08 01:49:56 +0800

    Add files via upload
    Udemy - Complete C# Masterclass

---
+++ Exercise 7_UsingStaticMethods.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int DoubleValue(int number)
+        {
+            // TODO: Implement the method to double the value
+            return number + number;
+        }
+
+        public static void Main(string[] args)
+        {
+            // TODO: Call DoubleValue and print the result
+            Console.WriteLine(DoubleValue(10));
+        }
+    }
+}
