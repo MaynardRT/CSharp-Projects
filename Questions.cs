80ff605e48505634444ac2dd114ed21e359920db
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2024-06-10 02:36:44 +0800

    Add files via upload
    -commit

---
+++ Questions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+    internal class Questions
+    {
+        public string QuestionText { get; set; }
+
+        public string[] Answers { get; set; }
+
+        public int CorrectAnswersIndex { get; set; }
+
+        public Questions(string questionText, string[] answer, int correctAnswerIndex)
+        {
+            QuestionText = questionText;
+            Answers = answer;
+            CorrectAnswersIndex = correctAnswerIndex;
+        }
+
+        public bool isCorrect(int choice) { 
+            return CorrectAnswersIndex == choice;
+        }
+    }
+}
