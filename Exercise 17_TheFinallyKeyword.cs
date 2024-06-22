298d9edf22ac20731d938d344a953a256629d0cb
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2024-06-22 23:57:00 +0800

    Add files via upload
    Udemy - Complete C# Masterclass

---
+++ Exercise 17_TheFinallyKeyword.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public void PrintWithFinally()
+        {
+            // TODO
+            try {
+                Console.WriteLine("Trying...");
+            } catch {
+                
+            } finally {
+                 Console.WriteLine("Finally executed.");
+            }
+        }
+    }
+}
