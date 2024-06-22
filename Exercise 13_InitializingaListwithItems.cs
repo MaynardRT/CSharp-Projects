3fcbf724adbd18237cfc6115af83a90bc9936f5a
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2024-06-22 23:53:27 +0800

    Add files via upload
    Udemy - Complete C# Masterclass

---
+++ Exercise 13_InitializingaListwithItems.cs
@@ -0,0 +1,20 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public void PrintNumbers()
+        {
+            // TODO
+            List<int> integersList = new List<int> {1,2,3};
+            
+            foreach (int i in integersList) {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
+
