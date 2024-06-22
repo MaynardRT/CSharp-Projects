e2f79e0c628b70648791c7786d4b9969529c5706
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2024-06-22 23:56:00 +0800

    Add files via upload
    Udemy - Complete C# Masterclass

---
+++ Exercise 15_UsingaComplexObjectastheValueofaDictionary.cs
@@ -0,0 +1,25 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public void PrintDictionaryValues()
+        {
+            // TODO
+            Dictionary<int, int> My_dict1 =  
+                       new Dictionary<int, int>();
+                       My_dict1.Add(1, 1);
+                       My_dict1.Add(2, 2);
+                       My_dict1.Add(3, 3);       
+            foreach(KeyValuePair<int, int> element in My_dict1)
+            {
+              Console.Write("{0} ",
+                        element.Key, element.Value);
+            }
+        }
+    }
+}
+
